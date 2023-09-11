2f83d7abf13d84567d4a29e90c696787c30a86c2
chr1swill <127428764+chr1swill@users.noreply.github.com>
2023-09-11 11:18:07 -0400

    feat - add function to randomly get computer choice

--- script.js
+++ script.js
@@ -13,3 +13,8 @@ const getScissors = () => {
 const getRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+const getComputerChoice = () => {
+    const choices = [getRock(), getPaper(), getScissors()];
+    return choices[getRandomNumber(0, 2)];
+}
