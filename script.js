21559ffd26fb0911595c8563c3985428cc4ed926
chr1swill <127428764+chr1swill@users.noreply.github.com>
2023-09-11 12:17:51 -0400

    feat - add function to get player choice

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ const getComputerChoice = () => {
     const choices = [getRock(), getPaper(), getScissors()];
     return choices[getRandomNumber(0, 2)];
 }
+
+const getPlayerChoice = (choice) => {
+    if (typeof choice !== 'string') {
+        console.log("Please enter a string: ", choice);
+    } else {
+        const lowerCaseChoice = choice.toLowerCase();
+        if (lowerCaseChoice !== 'rock' && lowerCaseChoice !== 'paper' && lowerCaseChoice !== 'scissors') {
+            console.log("Please enter a valid choice: ", lowerCaseChoice);
+        } else if (lowerCaseChoice === "rock") {
+            return getRock();
+        } else if (lowerCaseChoice === "paper") {
+            return getPaper();
+        } else if (lowerCaseChoice === "scissors") {
+            return getScissors();
+        }
+    }
+}
