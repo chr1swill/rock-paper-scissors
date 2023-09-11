dd28311e9f219fc0950c48101c336608fdebbb66
chr1swill <127428764+chr1swill@users.noreply.github.com>
2023-09-11 11:12:39 -0400

    feat - add random number function

--- script.js
+++ script.js
@@ -9,3 +9,7 @@ const getPaper = () => {
 const getScissors = () => {
     return "scissors";
 }
+
+const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
