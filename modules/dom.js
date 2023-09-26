fadaca49138ece01553d9ec16f1270286c6a34ac
chr1swill <alexanderwilliams20003@gmail.com>
2023-09-26 09:07:51 -0400

    refactor - remove comments from dom.js

--- modules/dom.js
+++ modules/dom.js
@@ -1,7 +1,4 @@
-// display text saying to select input 
 export const displayText = document.getElementById("displayToPlayer");
-
-// get player selected input rock paper scissors
 export const selectRock = document.getElementById("selectRock");
 export const selectPaper = document.getElementById("selectPaper");
 export const selectScissors = document.getElementById("selectScissors");
